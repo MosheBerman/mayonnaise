5d96e2cc2f06d136f943be95899b8cf3b0cf496d
Moshe Berman <moshberm@gmail.com>
2012-12-02 10:48:20 -0500

    Renamed the SalesRecord class to Record.

---
+++ 3-LinkedLists/Record.h
@@ -0,0 +1,53 @@
+//
+//  SalesRecord.h
+//  3-LinkedLists
+//
+//  Created by Moshe Berman on 12/2/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#ifndef ____LinkedLists__SalesRecord__
+#define ____LinkedLists__SalesRecord__
+
+#include <iostream>
+
+enum kSalesRecordType {
+    kSalesRecordTypeSale = 0,
+    kSalesRecordTypeReciept = 1,
+    kSalesRecordTypePromotion
+    };
+
+class Record {
+    kSalesRecordType recordType;
+    int quantity;
+    int saleAmount;
+    double pricePerWidget;
+    
+    //  Parser Methods
+
+    kSalesRecordType recordTypeFromString(std::string);
+    int quantityFromString(std::string);
+    int saleAmountFromString(std::string);
+    double priceFromString(std::string);
+    
+public:
+    
+    Record();
+    Record(std::string record);
+    
+    //  Getters and setters
+    
+    kSalesRecordType getRecordType();
+    void setRecordType(kSalesRecordType);
+    
+    int getQuantity();
+    void setQuantity(int);
+    
+    int getSaleAmount();
+    void setSaleAmount(int);
+    
+    double getPricePerWidget();
+    void setPricePerWidget(int);
+};
+
+#endif /* defined(____LinkedLists__SalesRecord__) */
