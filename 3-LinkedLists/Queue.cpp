9b2dee2d7159ba4b62fc62fa4e353b0e106438e9
Moshe Berman <moshberm@gmail.com>
2012-12-02 08:20:45 -0500

    Began implementing a Queue class which is capable of managing sales and order.

--- 3-LinkedLists/Queue.cpp
+++ 3-LinkedLists/Queue.cpp
@@ -7,3 +7,105 @@
 //
 
 #include "Queue.h"
+
+void Queue::setPromotionRate(double _promotionRate){
+    //  A negative "promotion" rate
+    // could mean a temporary markup.
+    promotionRate = _promotionRate;
+}
+
+int Queue::getNumberOfPromotions(){
+    return numberOfPromotionsRemaining;
+}
+
+void Queue::beginPromotion(){
+    numberOfPromotionsRemaining = 2;
+}
+
+void Queue::receiveNumWidgetsAtWidgetsAtPrice(int numberOfWidgets, double price){
+    Widget widget = Widget(numberOfWidgets, price);
+    widgets.push_back(widget);
+}
+
+void Queue::sellNumberOfWidgets(int numberOfWidgetsToSell, double price){
+    
+    //
+    //  If there are no widgets left, print out
+    //  the message mentioned in the spec,
+    //  and return.
+    //
+    
+    if (widgets.empty()) {
+        std::cout << "remainder " << numberOfWidgetsToSell << " not available." << std::endl;
+        return;
+    }
+    
+    //
+    //  If there are more widgets, pull them off of the queue.
+    //
+    
+    Widget nextBatchOfWidgets = widgets.front();
+    
+    //
+    //  Get the price per widget for this batch
+    //
+    
+    double pricePerItem = nextBatchOfWidgets.getPrice();
+    double quantityAvailable = quantityAvailableForDesiredQuantity(nextBatchOfWidgets, numberOfWidgetsToSell);
+
+    //  Add the price up.
+    
+    double priceOfBatch = priceForNWidgetsAtPricePerItem(quantityAvailable, pricePerItem);
+    price += priceOfBatch;
+    
+    
+  
+}
+
+//
+//  Check the current batch for widgets.
+//  If there's enough, take those and subtract them,
+//  otherwise, 
+//
+
+double Queue::quantityAvailableForDesiredQuantity(Widget batch, int numberOfWidgetsToSell) {
+    
+    double quantityAvailable = batch.getQuantity();
+    
+    if (quantityAvailable >= numberOfWidgetsToSell) {
+        quantityAvailable = numberOfWidgetsToSell;
+    }
+    
+    //
+    //  If we're using all of the widgets,
+    //  there's no more in the batch, so pop it.
+    //
+    
+    else{
+        widgets.pop_front();
+    }
+    
+    //  Update the quantity
+    batch.setQuantity(quantityAvailable - numberOfWidgetsToSell);
+    
+    return quantityAvailable;
+}
+
+//
+//  Calculate the price of the batch...
+//
+
+double Queue::priceForNWidgetsAtPricePerItem(int n, double pricePerItem){
+    
+    double priceOfBatch = pricePerItem * n;
+    priceOfBatch *= 1.3;   //  Apply the 30 percent markup
+    
+    //  ... apply the promotion if applicable.
+    
+    if (numberOfPromotionsRemaining > 0) {
+        priceOfBatch *= 1-(promotionRate/100);
+        numberOfPromotionsRemaining--;
+    }
+    
+    return priceOfBatch;
+}
--- 3-LinkedLists/Queue.h
+++ 3-LinkedLists/Queue.h
@@ -10,5 +10,31 @@
 #define ____LinkedLists__Queue__
 
 #include <iostream>
+#include <queue>
+#include "Widget.h"
+
+class Queue {
+    std::deque<Widget> widgets;
+    int numberOfPromotionsRemaining;
+    double promotionRate;
+public:
+    
+    Queue():numberOfPromotionsRemaining(0), promotionRate(0.0){};
+    
+    //  Getters and setters
+    
+    void setPromotionRate(double);
+    int getNumberOfPromotions();
+    void beginPromotion();
+    
+    //  Buy and sell widgets
+    
+    void receiveNumWidgetsAtWidgetsAtPrice(int, double);
+    void sellNumberOfWidgets(int, double);  //  Pass a price along so we can recurse
+
+    //  Sale helper methods
+    double quantityAvailableForDesiredQuantity(Widget, int);
+    double priceForNWidgetsAtPricePerItem(int, double);
+};
 
 #endif /* defined(____LinkedLists__Queue__) */
