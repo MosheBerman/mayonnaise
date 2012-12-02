a61752150939741ce9f26e566d5bc5275f24a6d8
Moshe Berman <moshberm@gmail.com>
2012-12-02 10:01:26 -0500

    Added an explanatory comment to the Sale class.

--- 3-LinkedLists/Sale.h
+++ 3-LinkedLists/Sale.h
@@ -6,6 +6,20 @@
 //  Copyright (c) 2012 Moshe Berman. All rights reserved.
 //
 
+/*
+
+ This class represents a *part* of
+ a sale of widgets, since different
+ pieces of stock have varying prices.
+ 
+ Each batch of widgets that is sold
+ is put into a "Sale" instance and 
+ then read out at the end of the 
+ sellNumberOfWidgets() function in
+ the Queue class.
+ 
+ */
+
 #ifndef ____LinkedLists__Sale__
 #define ____LinkedLists__Sale__
 
