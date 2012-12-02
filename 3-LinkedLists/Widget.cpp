6f6ae7ef20090be32958407dbb7a0da70a55833c
Moshe Berman <moshberm@gmail.com>
2012-12-02 06:48:49 -0500

    Added Widget class.

---
+++ 3-LinkedLists/Widget.cpp
@@ -0,0 +1,9 @@
+//
+//  Widget.cpp
+//  3-LinkedLists
+//
+//  Created by Moshe Berman on 12/2/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#include "Widget.h"
---
+++ 3-LinkedLists/Widget.h
@@ -0,0 +1,14 @@
+//
+//  Widget.h
+//  3-LinkedLists
+//
+//  Created by Moshe Berman on 12/2/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#ifndef ____LinkedLists__Widget__
+#define ____LinkedLists__Widget__
+
+#include <iostream>
+
+#endif /* defined(____LinkedLists__Widget__) */
