618f2c7c3e8c657afb22b7d585e3cb259f8813d0
Moshe Berman <moshberm@gmail.com>
2012-12-02 20:36:04 -0500

    Implemented the main class.

--- 3-LinkedLists/main.cpp
+++ 3-LinkedLists/main.cpp
@@ -9,11 +9,64 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+#include "FileIO.h"
+
+#include "Record.h"
+#include "Sale.h"
+#include "StoreKeeper.h"
+#include "Widget.h"
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream records;
+    std::string fileName = "Input.txt";
+    if(!openInputStream(records, fileName)){
+        cout << "Couldn't open " << fileName << std::endl;
+        throw runtime_error("Couldn't open " + fileName);
+    }
+    
+    std::string line;
+    
+    //
+    //  Skip the first two lines, since
+    //  they contain unusable data and
+    //  whitespace.
+    //
+    
+    getline(records, line);
+    getline(records, line);
+
+    //
+    //  Create a storekeeper instance
+    //  to process sales as we go.
+    //
+    
+    Storekeeper *shop = new Storekeeper();
+    
+    //
+    //  Process the input files.
+    //
+    
+    while (getline(records, line)) {
+        
+        Record r = Record(line);
+        
+        if (r.getRecordType() == kRecordTypePromotion) {
+            shop->setPromotionRate(r.getSaleAmount());
+            shop->beginPromotion();
+        }
+        else if(r.getRecordType() == kRecordTypeSale){
+            shop->sellWidgets(r.getQuantity());
+        }
+        else if(r.getRecordType() == kRecordTypeReciept){
+            shop->receiveNumWidgetsAtWidgetsAtPrice(r.getQuantity(), r.getPricePerWidget());
+        }
+    }
+    
+    shop->printOverstock();
+    
     return 0;
 }
